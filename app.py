1e980542bb23f316d470b07f0dd0834d52a626ef
Sankarikm126 <sankari@savemyexams.co.uk>
2025-07-14 16:20:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,3 @@ def index():
                 message = "Upload to Dropbox completed!"
 
     return render_template("index.html", message=message)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
